d8e3b57baa175e885d433864e5c98d0a88745d87
GwangJin Byeon <qusrhkdwls45@gmail.com>
2022-12-02 16:44:41 +0900

    history

--- test/ts/basicSpec.ts
+++ test/ts/basicSpec.ts
@@ -1,11 +1,41 @@
 import { expect } from "chai"
+import { 
+    MosaicNode,
+    MosaicKey,
+    MosaicParent
+} from '../../src/types'
+
+// type _MosaicNode<T> = MosaicParent<T> | T;
 
 describe('ts type', () => {
-    it('test', ()=>{
-        function test(props:string):string{
-            return props;
-        };
 
-        expect(test('q')).to.equal('q');
+    it('MosaicNode 제넥릭 number', ()=>{
+        function test(props:MosaicNode<number>):MosaicNode<number>{
+            return props;
+        }
+        
+        let result = test(1);
+
+        expect(result).to.equal(1);
+    })
+
+    it('MosaicNode 제넥릭 string', ()=>{
+        function test(props:MosaicNode<string>):MosaicNode<string>{
+            return props;
+        }
+        
+        let result = test('1');
+
+        expect(result).to.equal('1');
+    })
+
+    it('MosaicNode 제넥릭 MosaicNode', ()=>{
+        function test(props:MosaicNode<string>):MosaicNode<string>{
+            return props;
+        }
+        
+        let result = test('1');
+
+        expect(result).to.equal('1');
     })
 })
