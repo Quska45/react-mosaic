53be84b7396a1f3651746e57445fdcd087dc626f
GwangJin Byeon <qusrhkdwls45@gmail.com>
2022-12-03 15:34:56 +0900

    MosaicNode Class 인스턴트 사용 테스트 코드 작성

--- test/ts/basicSpec.ts
+++ test/ts/basicSpec.ts
@@ -1,15 +1,16 @@
 import { expect } from "chai"
 import { 
     MosaicNode,
+    MosaicParent,
     MosaicKey,
-    MosaicParent
+    MosaicDirection
 } from '../../src/types'
 
 // type _MosaicNode<T> = MosaicParent<T> | T;
 
 describe('ts type', () => {
 
-    it('MosaicNode 제넥릭 number', ()=>{
+    it('MosaicNode 제네릭타입 number', ()=>{
         function test(props:MosaicNode<number>):MosaicNode<number>{
             return props;
         }
@@ -19,7 +20,7 @@ describe('ts type', () => {
         expect(result).to.equal(1);
     })
 
-    it('MosaicNode 제넥릭 string', ()=>{
+    it('MosaicNode 제네릭타입 string', ()=>{
         function test(props:MosaicNode<string>):MosaicNode<string>{
             return props;
         }
@@ -29,13 +30,56 @@ describe('ts type', () => {
         expect(result).to.equal('1');
     })
 
-    it('MosaicNode 제넥릭 MosaicNode', ()=>{
-        function test(props:MosaicNode<string>):MosaicNode<string>{
+    it('MosaicNode 제네릭타입 MosaicNode', ()=>{
+        function test(props:MosaicNode<number>):MosaicNode<number>{
             return props;
         }
-        
-        let result = test('1');
 
-        expect(result).to.equal('1');
+        // 객체의 타입이 지정 되어 있어야 함
+        const mosaicNode:MosaicNode<number> = {
+            direction: 'row',
+            first: 1,
+            second: {
+                direction: 'column',
+                first: 2,
+                second: 3,
+            },
+            splitPercentage: 40,
+        };
+        
+        let result = test( mosaicNode );
+
+        expect(result).to.equal( mosaicNode );
+    })
+    it('MosaicNode 제네릭 MosaicNode + 인터페이스를 구현하는 클래스 만들어 쓰기', ()=>{
+        function test(props:MosaicNode<number>):MosaicNode<number>{
+            return props;
+        }
+
+        // 이렇게 생성해서 쓰는게 가능하긴 하지만 왜 이렇게 이상하게 나오냐...
+        class MosaicNodeClass<T extends MosaicKey> implements MosaicParent<T> {
+            direction: MosaicDirection;
+            first: MosaicNode<T>;
+            second: MosaicNode<T>;
+            splitPercentage?: number;
+
+            constructor( 
+                direction: MosaicDirection,
+                first: MosaicNode<T>,
+                second: MosaicNode<T>,
+                splitPercentage: number 
+            ){
+                 this.direction = direction;
+                 this.first = first;
+                 this.second = second;
+                 this.splitPercentage = splitPercentage;
+            }
+        }
+
+        const mosaicNodeClass:MosaicNode<number> = new MosaicNodeClass( 'row', 1, 1, 20 );
+        
+        let result = test( mosaicNodeClass );
+
+        expect(result).to.equal( mosaicNodeClass );
     })
 })
